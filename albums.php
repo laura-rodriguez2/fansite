75fe58fe9227cae4727007aa6f744e04f510b79c
Laura <laura.rodriguez@laplateforme.io>
2022-11-04 15:22:29 +0100

    ajout liens albums

--- albums.php
+++ albums.php
@@ -20,30 +20,32 @@
     <h1 class="h1_albums">Albums</h1> 
   <section id="albums">
     <section class="gallery">
-
-      <img src="https://concord.com/wp-content/uploads/2021/09/Starset_Horizons.jpg" width="300" height="300">
-
+<a href="https://www.youtube.com/watch?v=qr4HxlCx4hA&list=PLsBWlr67U5NuM3iB5hCcSUOei5p9CviwF&ab_channel=starsetonline" target="blank">
+      <img src="https://concord.com/wp-content/uploads/2021/09/Starset_Horizons.jpg" width="300" height="300" alt="cover de l'album">
+</a>
       <section class="desc">Horizons - 2021</section>
     </section>
 
     <section class="gallery">
-
-      <img src="https://concord.com/wp-content/uploads/2019/08/Starset-SyntesisCoverDSP.jpg" width="300" height="300">
-
+    <a href="https://www.youtube.com/watch?v=ZXng1JE9dmk&list=PLzLnsw-yVvLhEX0VIpNj8qCVrJ4JM5Kno&ab_channel=starsetonline" target="blank">
+      <img src="https://concord.com/wp-content/uploads/2019/08/Starset-SyntesisCoverDSP.jpg" width="300" height="300" alt="cover de l'album">
+      </a>
       <section class="desc">Divisions - 2019</section>
     </section>
 
     <section class="gallery">
+    <a href="https://www.youtube.com/watch?v=iRq4BqwyjBo&list=PLRPMu7rvR8Ldvkij69fXMvHD88rTloiJO&ab_channel=WinglessAngel95" target="blank">
 
-      <img src="https://concord.com/wp-content/uploads/2017/12/Starset-Vessels.jpg" width="300" height="300">
-
+      <img src="https://concord.com/wp-content/uploads/2017/12/Starset-Vessels.jpg" width="300" height="300" alt="cover de l'album">
+      </a>
       <section class="desc">Vessels - 2017</section>
     </section>
 
     <section class="gallery">
-  
-      <img src="https://townsquare.media/site/366/files/2014/06/Starset.jpg" width="300" height="300">
+    <a href="https://www.youtube.com/watch?v=tW_FkaXeSwc&list=PL6BsulcnTjtv3k03say7Z1RjihleUU5nE&ab_channel=starsetVEVO" target="blank">
 
+      <img src="https://townsquare.media/site/366/files/2014/06/Starset.jpg" width="300" height="300" alt="cover de l'album">
+      </a>
       <section class="desc">Transmissions - 2014</section>
     </section>
     </section>
